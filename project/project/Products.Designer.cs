63c15b9eb098dc8959c12fc0f2e0f90be8775a1c
Thanusan256 <67289032+Thanusan256@users.noreply.github.com>
2020-08-07 22:01:55 +0530

    update

--- project/project/Products.Designer.cs
+++ project/project/Products.Designer.cs
@@ -34,19 +34,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(254, 88);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.Size = new System.Drawing.Size(252, 42);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Product page";
+            this.label1.Text = "Order Product";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Products
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Products";
-            this.Size = new System.Drawing.Size(593, 572);
+            this.Size = new System.Drawing.Size(690, 502);
             this.ResumeLayout(false);
             this.PerformLayout();
 
