fbe26f60d7f211730fed55bb3d394a7f6846eba8
Thanusan256 <67289032+Thanusan256@users.noreply.github.com>
2020-08-07 13:41:26 +0530

    Update Form1.Designer.cs

--- project/project/Form1.Designer.cs
+++ project/project/Form1.Designer.cs
@@ -50,7 +50,6 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 48);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Thanusan";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -66,13 +65,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(1211, 608);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Point of sales";
             this.ResumeLayout(false);
 
         }
