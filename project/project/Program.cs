ef03c801ca51cad1df6f2de1e718c1a124bdda79
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2020-08-26 12:29:25 +0530

    update

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -17,6 +17,7 @@ namespace project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+            
         }
     }
 }
